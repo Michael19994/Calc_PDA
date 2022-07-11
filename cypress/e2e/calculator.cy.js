aee0d88ae6de20c60b5606fe6a925c41f53298c5
user <user@users-MacBook-Pro.local>
2022-07-11 21:44:17 +0100

    2nd commit

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -23,6 +23,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '5');
   })
+
   it('should be able to process multiple operations be chained together', () => {
     cy.get('#number3').click();
     cy.get('#operator-add').click();
@@ -32,6 +33,7 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '15');
   })
+
   it('should be able to display the output as expected for a range of numbers', () => {
     cy.get('#number1').click();
     cy.get('#number0').click();
@@ -46,8 +48,33 @@ describe("Calculator", () => {
     cy.get('#number0').click();
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '10000000');
-
   })
+
+  it('should be able to display the output number decimals', ()  => {
+    cy.get('#number1').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '0.3333333333333333');
+  })
+
+  it('should be able to display the output postive numbers', ()  => {
+    cy.get('#number1').click();
+    cy.get('#operator-add').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '4');
+  })
+
+  it('should be able to display the output negative numbers', ()  => {
+    cy.get('#number1').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-2');
+  })
+
+
   it('should be able to alter the code to peform in exceptional circumstances', () => {
     cy.get('#number0').click();
     cy.get('#operator-divide');
